d5dba46a5b6ff00a0dcee722ccd0558cf2752dd6
Sebastian Kusnier <sebastian@kusnier.net>
2016-11-30 23:21:49 +0100

    Improve the controller handling
    - Add support for SCSI controller
    - Use always the last port
    - On detach search the port with the location from config file
    - Add support for ~ (use real path)
    
    Fixes #55

--- lib/vagrant-persistent-storage/providers/virtualbox/driver/base.rb
+++ lib/vagrant-persistent-storage/providers/virtualbox/driver/base.rb
@@ -1,3 +1,4 @@
+require "log4r"
 require 'pathname'
 
 module VagrantPlugins
@@ -16,7 +17,7 @@ module VagrantPlugins
         end
 
         def create_storage(location, size)
-          execute("createhd", "--filename", location, "--size", "#{size}")
+          execute("createhd", "--filename", File.expand_path(location), "--size", "#{size}")
         end
 
         def attach_storage(location)
@@ -25,30 +26,42 @@ module VagrantPlugins
             controller_name = "SATA Controller"
           end
 
+          location_realpath = File.expand_path(location)
+
           if controller_name.start_with?("IDE")
-              execute("storageattach", @uuid, "--storagectl", get_controller_name, "--port", "1", "--device", "0", "--type", "hdd", "--medium", "#{location}")
+              execute("storageattach", @uuid, "--storagectl", get_controller_name, "--port", "4", "--device", "0", "--type", "hdd", "--medium", "#{location_realpath}")
+          elsif controller_name.start_with?("SCSI")
+              execute("storageattach", @uuid, "--storagectl", get_controller_name, "--port", "15", "--device", "0", "--type", "hdd", "--medium", "#{location_realpath}")
           else
-              execute("storageattach", @uuid, "--storagectl", get_controller_name, "--port", "1", "--device", "0", "--type", "hdd", "--medium", "#{location}", "--hotpluggable", "on")
+              execute("storageattach", @uuid, "--storagectl", get_controller_name, "--port", "4", "--device", "0", "--type", "hdd", "--medium", "#{location_realpath}", "--hotpluggable", "on")
           end
 
 
         end
 
         def detach_storage(location)
-          persistent_storage = read_persistent_storage()
-          if location and persistent_storage and persistent_storage != "none" and identical_files(persistent_storage, location)
-            execute("storageattach", @uuid, "--storagectl", get_controller_name, "--port", "1", "--device", "0", "--type", "hdd", "--medium", "none")
+          location_realpath = File.expand_path(location)
+          persistent_storage_data = read_persistent_storage(location_realpath)
+          if location and persistent_storage_data and identical_files(persistent_storage_data.location, location_realpath)
+              execute("storageattach", @uuid, "--storagectl", persistent_storage_data.controller, "--port", persistent_storage_data.port, "--device", "0", "--type", "hdd", "--medium", "none")
           end
         end
 
-        def read_persistent_storage()
+        def read_persistent_storage(location)
           ## Ensure previous operations are complete - bad practise yes, not sure how to avoid this:
           sleep 3
+          storage_data = nil
+          controller_name = get_controller_name
           info = execute("showvminfo", @uuid, "--machinereadable", :retryable => true)
           info.split("\n").each do |line|
-            return $1.to_s if line =~ /^"#{get_controller_name}-1-0"="(.+?)"$/
+              tmp_storage_data = nil
+              tmp_storage_data = PersistentStorageData.new(controller_name, $1, $3) if line =~ /^"#{controller_name}-(\d+)-(\d+)"="(.*)"/
+
+              if !tmp_storage_data.nil? and tmp_storage_data.location != 'none' and identical_files(File.expand_path(location), tmp_storage_data.location)
+                  storage_data = tmp_storage_data
+              end
           end
-          nil
+          return storage_data
         end
 
         def identical_files(file1, file2)
@@ -61,7 +74,7 @@ module VagrantPlugins
           info = execute("showvminfo", @uuid, "--machinereadable", :retryable => true)
           info.split("\n").each do |line|
             controllers[$1] = $2 if line =~ /^storagecontrollername(\d+)="(.*)"/
-            controller_number = $1 if line =~ /^storagecontrollertype(\d+)="(IntelAhci|PIIX4)"/
+            controller_number = $1 if line =~ /^storagecontrollertype(\d+)="(IntelAhci|PIIX4|LsiLogic)"/
           end
 
           if controller_number.nil?
@@ -69,6 +82,19 @@ module VagrantPlugins
           end
 
           return controllers[controller_number]
+        end
+
+        class PersistentStorageData
+
+          attr_accessor :controller
+          attr_accessor :port
+          attr_accessor :location
+
+          def initialize(controller,port,location)
+            @controller = controller
+            @port = port
+            @location = location
+          end
         end
 
       end
